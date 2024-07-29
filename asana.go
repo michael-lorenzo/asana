4dd1583927c825e2391c4edf01b25e190ddf9b1a
Michael Lorenzo <61134276+michael-lorenzo@users.noreply.github.com>
2024-07-29 10:12:38 -0700

    Use viper

--- asana.go
+++ asana.go
@@ -2,23 +2,20 @@ package main
 
 import (
 	"bytes"
+	_ "embed"
 	"encoding/json"
 	"flag"
+	"github.com/spf13/viper"
 	"log"
 	"net/http"
 	"os"
-
-	"github.com/BurntSushi/toml"
+	"path/filepath"
 )
-
-type Config struct {
-	PersonalAccessToken string `toml:"personal_access_token"`
-	WorkspaceGid        string `toml:"workspace_gid"`
-}
 
 type Payload struct {
 	Data Data `json:"data"`
 }
+
 type Data struct {
 	Name      string `json:"name"`
 	Assignee  string `json:"assignee"`
@@ -26,52 +23,77 @@ type Data struct {
 	Notes     string `json:"notes"`
 }
 
+//go:embed asana.toml
+var emptyConfig []byte
+
 func main() {
-	f, err := os.Open("asana.toml")
-	if err != nil {
-		log.Fatalln("Error opening config file: asana.toml")
-	}
-	d := toml.NewDecoder(f)
-	var c Config
-	_, err = d.Decode(&c)
-	if err != nil {
-		log.Fatalln("Error bad formatting in config file: asana.toml")
+	generateConfig := flag.Bool("generate", false, "Generate an empty config file")
+	flag.Parse()
+
+	if *generateConfig {
+		uhd, err := os.UserHomeDir()
+		if err != nil {
+			log.Fatal(err)
+		}
+		emptyConfigPath := filepath.Join(uhd, "asana.toml")
+		f, err := os.OpenFile(emptyConfigPath, os.O_WRONLY|os.O_CREATE|os.O_EXCL, 0o644)
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Println("Generating empty config file at", emptyConfigPath)
+		_, err = f.Write(emptyConfig)
+		if err != nil {
+			log.Fatal(err)
+		}
+		return
 	}
 
-	flag.Parse()
+	viper.SetConfigName("asana")
+	viper.SetConfigType("toml")
+	viper.AddConfigPath("$HOME")
+	viper.AddConfigPath(".")
+	err := viper.ReadInConfig()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	if flag.NArg() == 0 {
-		log.Fatalln("Task must have a name")
+		log.Fatal("Task must have a name")
 	}
 
 	data := Payload{
 		Data: Data{
 			Name:      flag.Arg(0),
 			Assignee:  "me",
-			Workspace: c.WorkspaceGid,
+			Workspace: viper.GetString("workspace_gid"),
 			Notes:     flag.Arg(1),
 		},
 	}
 	payloadBytes, err := json.Marshal(data)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 	body := bytes.NewReader(payloadBytes)
 
 	req, err := http.NewRequest("POST", "https://app.asana.com/api/1.0/tasks", body)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 	req.Header.Set("Accept", "application/json")
-	req.Header.Set("Authorization", "Bearer "+c.PersonalAccessToken)
+	req.Header.Set("Authorization", "Bearer "+viper.GetString("personal_access_token"))
 	req.Header.Set("Content-Type", "application/json")
 
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
-		log.Fatalln("Error connecting to Asana API")
+		log.Fatal(err)
 	}
-	defer resp.Body.Close()
+	defer func() {
+		if err := resp.Body.Close(); err != nil {
+			log.Fatal(err)
+		}
+	}()
 
 	if resp.StatusCode != 201 {
-		log.Fatalf("Error status code %d\n", resp.StatusCode)
+		log.Fatalf("Status code %v\n", resp.StatusCode)
 	}
 }
